44c6581ae873dd0b4b3200882fc14d1272f54235
1ruk@913 <qkralswns1998@gmail.com>
2024-10-23 12:40:32 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 5.73 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,19 @@
+function solution(s) {
+    let answer;
+    sArray = [...s.split(" ")];
+    console.log(sArray);
+    let answerArray = sArray.map((items) => {
+        let arrofItems = [...items];
+        for (let i = 0; i < items.length; i++) {
+            if (i % 2 === 0) {
+                arrofItems[i] = arrofItems[i].toUpperCase();
+            } else {
+                arrofItems[i] = arrofItems[i].toLowerCase();
+            }
+        }
+        let newItems = arrofItems.join("");
+        return newItems;
+    });
+    console.log(answerArray);
+    return (answer = answerArray.join(" "));
+}
