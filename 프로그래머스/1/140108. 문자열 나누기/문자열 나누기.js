b9c9fd180c0741366080bd28d73805cfca7876b2
1ruk@913 <qkralswns1998@gmail.com>
2024-11-21 09:01:30 +0900

    [level 1] Title: 문자열 나누기, Time: 23.15 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -1,13 +1,10 @@
 function solution(s) {
-    let count = 0; // 분리된 문자열 개수
-    let i = 0; // 현재 위치
-
+    let count = 0; 
+    let i = 0; 
     while (i < s.length) {
-        let x = s[i]; // 현재 분리 기준 문자
+        let x = s[i]; 
         let xCount = 0;
         let otherCount = 0;
-
-        // 두 그룹의 횟수가 같아질 때까지 반복
         for (let j = i; j < s.length; j++) {
             if (s[j] === x) {
                 xCount++;
@@ -15,15 +12,12 @@ function solution(s) {
                 otherCount++;
             }
 
-            // 조건 충족 시 문자열 분리
             if (xCount === otherCount) {
                 count++;
-                i = j + 1; // 다음 분리 시작 지점으로 이동
+                i = j + 1; 
                 break;
             }
         }
-
-        // 조건 충족이 불가능한 경우 남은 문자열을 하나로 처리
         if (xCount !== otherCount) {
             count++;
             break;
