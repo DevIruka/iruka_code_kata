354e3726ceca950824322e1b30c0e9c350d5edfe
1ruk@913 <qkralswns1998@gmail.com>
2024-11-27 08:34:11 +0900

    [level 1] Title: 둘만의 암호, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,8 +1,6 @@
 function solution(s, skip, index) {
     let answer = "";
     let alphabets = "abcdefghijklmnopqrstuvwxyz".split("");
-
-    // skip에 포함된 알파벳 제거
     const skipArray = skip.split("");
     alphabets = alphabets.filter((char) => !skipArray.includes(char));
     
@@ -12,7 +10,7 @@ function solution(s, skip, index) {
         .split("")
         .map((char) => {
             let currentIndex = alphabets.indexOf(char);
-            let newIndex = (currentIndex + index) % maxLength; // 초과하는 경우도 처리
+            let newIndex = (currentIndex + index) % maxLength;
             return alphabets[newIndex];
         })
         .join("");
